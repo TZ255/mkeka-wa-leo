b20504c85819cab5d182cf810e4aa305091339b9
gshemdoe <georgehmariki@gmail.com>
2023-11-28 12:49:43 +0300

    updated again

--- bots/helen/fns/mkeka-1-2-3.js
+++ bots/helen/fns/mkeka-1-2-3.js
@@ -74,12 +74,12 @@ const sendMkeka3 = async (ctx, delay, bot, imp) => {
             let bwTZ = `https://mkekawaleo.com/betway-tz/register`
             let gsb = 'https://mkekawaleo.com/gsb-tz/register'
             let pm = `https://pmaff.com/?serial=61291818&creative_id=1788`
-            let ke = `https://mkekawaleo.com/22bet-ke/register`
+            let ke = `https://mkekawaleo.com/1xbet/register`
             let ug = `https://mkekawaleo.com/gsb-ug/register`
             let prm = `https://mkekawaleo.com/premierbet/register`
             let tz_888 = `https://media.888africa.com/C.ashx?btag=a_416b_310c_&affid=356&siteid=416&adid=310&c=`
 
-            let finaText = txt + `<b>🔥 Total Odds: ${odds}</b>\n\n▬▬▬▬▬▬▬▬▬▬▬▬\n\nMkeka huu umeandaliwa 888Bet\n\n<i>» Jisajili na upokee Tsh. 3,000 bure pamoja na bonus ya 100% kwa deposit ya kwanza</i> \n\nKama bado huna account,\n\n<b>✓ Jisajili Hapa \n\n👤 (Tanzania 🇹🇿)</b>\n<a href="${tz_888}">https://m.888bet.tz/register\nhttps://m.888bet.tz/register</a>\n▬\n<b>👤 (Kenya 🇰🇪)</b>\n<a href="${ke}">https://22bet.co.ke/register</a>\n▬\n<b>👤 (Uganda 🇺🇬)</b>\n<a href="${ug}">https://m.gsb.ug/register</a>\n\n©MkekaWaLeo`
+            let finaText = txt + `<b>🔥 Total Odds: ${odds}</b>\n\n▬▬▬▬▬▬▬▬▬▬▬▬\n\nMkeka huu umeandaliwa 888Bet\n\n<i>» Jisajili na upokee Tsh. 3,000 bure pamoja na bonus ya 100% kwa deposit ya kwanza</i> \n\nKama bado huna account,\n\n<b>✓ Jisajili Hapa \n\n👤 (Tanzania 🇹🇿)</b>\n<a href="${tz_888}">https://m.888bet.tz/register\nhttps://m.888bet.tz/register</a>\n▬\n<b>👤 (Kenya 🇰🇪)</b>\n<a href="${ke}">https://1xbet.co.ke/register</a>\n▬\n<b>👤 (Uganda 🇺🇬)</b>\n<a href="${ug}">https://m.gsb.ug/register</a>\n\n©MkekaWaLeo`
 
             await ctx.reply(finaText, { parse_mode: 'HTML', disable_web_page_preview: true })
         } else if (keka.length > 0 && (tzHrs >= 22)) {
