a4d3d2b965dfcf9cc29dd5aef579e8dc8694d02a
gshemdoe <georgehmariki@gmail.com>
2024-01-15 01:23:43 +0300

    1xbet link updated

--- routes/get.js
+++ routes/get.js
@@ -142,7 +142,7 @@ router.get('/:comp/register', async (req, res) => {
         betika: `https://record.betikapartners.com/_xq39yU84NJbUOsjNOfgKeWNd7ZgqdRLk/1/`,
         gsb_ug: `https://track.africabetpartners.com/visit/?bta=35468&nci=5740`,
         ke_22bet: `https://welcome.toptrendyinc.com/redirect.aspx?pid=77675&bid=1490&lpid=48`,
-        ke_1xbet: `https://refpa4948989.top/L?tag=d_2869291m_1573c_kenya&site=2869291&ad=1573`,
+        ke_1xbet: `https://refpa4293501.top/L?tag=d_2869291m_2528c_&site=2869291&ad=2528`,
         ug_22bet: `https://welcome.toptrendyinc.com/redirect.aspx?pid=77677&bid=1489&lpid=34`,
         tz_22bet: `https://welcome.toptrendyinc.com/redirect.aspx?pid=77678&bid=1491&lpid=290`,
         tz_888: `http://media.888africa.com/C.ashx?btag=a_416b_310c_&affid=356&siteid=416&adid=310&c=`
