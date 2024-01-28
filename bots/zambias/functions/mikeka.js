abdf143dcfb38278b544071de492951a6a5fe8d5
gshemdoe <georgehmariki@gmail.com>
2024-01-28 07:35:24 +0300

    updated

--- bots/zambias/functions/mikeka.js
+++ bots/zambias/functions/mikeka.js
@@ -27,7 +27,7 @@ const sendMkeka3 = async (ctx, delay, bot, imp) => {
             let zm = `https://track.africabetpartners.com/visit/?bta=35468&nci=5976&utm_campaign=zambia`
             let zm_short = `https://is.gd/register_gsb_zambia`
 
-            let finaText = txt + `<b>🔥 Total Odds: ${odds.toLocaleString('en-US')}</b>\n\n▬▬▬▬▬▬▬▬▬▬▬▬\n\nThese bet options are available at <b>22bet</b> with 200% bonus on your first deposit\n\n<b>✓ Register Here </b>\n\n<b>👤 (Kenya 🇰🇪)</b>\n<b><a href="${ke}">https://22bet.co.ke/register\nhttps://22bet.co.ke/register</a></b>\n\n<b>👤 (Tanzania 🇹🇿)</b>\n<b><a href="${prm}">https://22bet.co.tz/register</a></b>\n\n\n•••`
+            let finaText = txt + `<b>🔥 Total Odds: ${Number(odds).toLocaleString('en-US')}</b>\n\n▬▬▬▬▬▬▬▬▬▬▬▬\n\nThese bet options are available at <b>22bet</b> with 200% bonus on your first deposit\n\n<b>✓ Register Here </b>\n\n<b>👤 (Kenya 🇰🇪)</b>\n<b><a href="${ke}">https://22bet.co.ke/register\nhttps://22bet.co.ke/register</a></b>\n\n<b>👤 (Tanzania 🇹🇿)</b>\n<b><a href="${prm}">https://22bet.co.tz/register</a></b>\n\n\n•••`
 
             await ctx.reply(finaText, { parse_mode: 'HTML', disable_web_page_preview: true })
         } else {
