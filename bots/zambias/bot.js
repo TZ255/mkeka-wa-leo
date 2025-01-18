63dd39c10d168db2606ae163f5e5433944b675f3
gshemdoe <georgehmariki@gmail.com>
2025-01-18 15:01:28 +0300

    done

--- bots/zambias/bot.js
+++ bots/zambias/bot.js
@@ -142,40 +142,7 @@ const myBotsFn = async (app) => {
             })
 
             bot.command('convo', async ctx => {
-                let convoBots = [
-                    "Kenya_Kuma_Kutombana_Bot", 
-                    "Kuma_Kinembe_Nairobi_Kisumu_Bot",
-                    "lugazi_sugar_mummybot"
-                ]
-                let admins = [imp.halot, imp.shemdoe]
-                if (admins.includes(ctx.chat.id) && convoBots.includes(ctx.me.username) && ctx.match) {
-                    let msg_id = Number(ctx.match.trim())
-                    let bads = ['deactivated', 'blocked', 'initiate', 'chat not found']
-                    try {
-                        let all_users = await usersModel.find({botname: ctx.me.username})
-                        await ctx.reply(`Starting broadcasting for ${all_users.length} users`)
-            
-                        all_users.forEach((u, i) => {
-                            setTimeout(() => {
-                                bot.api.copyMessage(u.chatid, imp.matangazoDB, msg_id)
-                                    .then(() => {
-                                        if (i === all_users.length - 1) {
-                                            ctx.reply('Nimemaliza conversation').catch(e => console.log(e.message))
-                                        }
-                                    })
-                                    .catch((err) => {
-                                        if (bads.some((b) => err?.message.toLowerCase().includes(b))) {
-                                            u.deleteOne()
-                                        } else { 
-                                            console.log(`🤷‍♂️ ${err.message}`) 
-                                        }
-                                    })
-                            }, i * 50) // 20 messages per second
-                        })
-                    } catch (err) {
-                        console.log(err?.message)
-                    }
-                }
+                //
             })
 
             bot.callbackQuery(['money', 'pussy'], async ctx => {
---
+++ bots/zambias/functions/convo.js
@@ -0,0 +1,3 @@
+const makeConvo = async () => {
+    
+}
