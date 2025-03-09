31a736321fdadddb2b2aa6c1708896495511de10
gshemdoe <georgehmariki@gmail.com>
2025-03-09 18:06:43 +0300

    updated

--- routes/fns/checking-betslip.js
+++ routes/fns/checking-betslip.js
@@ -79,31 +79,25 @@ const checking3MkekaBetslip = async (d) => {
         }
 
 
-        //################ slip 4 (DC - match.today) #########################
+        //################ slip 4 (Direct win - match.today) #########################
         let vip4 = await paidVipModel.find({ date: d, vip_no: 4 });
         if (vip4.length < 1) {
-            let home_win = ['2:0'];
-            let away_win = ['0:2'];
-            let direct_home = ['3:0', '4:0', '4:1']
-            let direct_away = ['0:3', '0:4', '1:4']
+            let direct_home = ['3:0', '4:0', '4:1', '5:0', '5:1']
+            let direct_away = ['0:3', '0:4', '1:4', '0:5', '1:5']
 
             let matches = await correctScoreModel.aggregate([
                 {
                     $match: {
                         siku: d, time: { $gte: '10:00' },
-                        tip: { $in: [...home_win, ...away_win, ...direct_away, ...direct_home] }
+                        tip: { $in: [...direct_away, ...direct_home] }
                     }
                 },
-                { $sample: { size: 5 } }
+                { $sample: { size: 3 } }
             ]);
 
             let transformedData = matches.map(doc => {
                 let newTip;
-                if (home_win.includes(doc.tip)) {
-                    newTip = "1X";
-                } else if (away_win.includes(doc.tip)) {
-                    newTip = "X2";
-                } else if (direct_home.includes(doc.tip)) {
+                if (direct_home.includes(doc.tip)) {
                     newTip = 'Home Win'
                 } else if (direct_away.includes(doc.tip)) {
                     newTip = 'Away Win'
@@ -128,7 +122,7 @@ const checking3MkekaBetslip = async (d) => {
         let vip5 = await paidVipModel.find({ date: d, vip_no: 5 })
         if (vip5.length < 1) {
             const copies = await correctScoreModel.aggregate([
-                { $match: { siku: d, time: {$gte: '10:00'} } },
+                { $match: { siku: d, time: { $gte: '10:00' } } },
                 // Add a field that splits the tip string and calculates total goals
                 {
                     $addFields: {
@@ -167,26 +161,43 @@ const checking3MkekaBetslip = async (d) => {
             }
         }
 
-        //################# slip 6 (Correct score) ###################################
-        // let vip6 = await paidVipModel.find({ date: d, vip_no: 6 });
-        // if (vip6.length < 1) {
-        //     const matches = await fameTipsModel.aggregate([
-        //         {
-        //             $match: {siku: d}
-        //         },
-        //         { $sample: { size: 2 } }
-        //     ]);
+        //################# slip 6 (Correct score double cha) ###################################
+        let vip6 = await paidVipModel.find({ date: d, vip_no: 6 });
+        if (vip6.length < 1) {
+            let home_win = ['2:0'];
+            let away_win = ['0:2'];
 
-        //     const transformedData = matches.map(doc => {
-        //         return {
-        //             date: doc.siku, time: doc.time, match: doc.match, league: doc.league, vip_no: 6, odd: '1', tip: doc.tip
-        //         };
-        //     });
+            let matches = await correctScoreModel.aggregate([
+                {
+                    $match: {
+                        siku: d, time: { $gte: '10:00' },
+                        tip: { $in: [...home_win, ...away_win] }
+                    }
+                },
+                { $sample: { size: 3 } }
+            ]);
 
-        //     if (transformedData.length > 0) {
-        //         await paidVipModel.insertMany(transformedData);
-        //     }
-        // }
+            let transformedData = matches.map(doc => {
+                let newTip;
+                if (home_win.includes(doc.tip)) {
+                    newTip = "1X";
+                } else if (away_win.includes(doc.tip)) {
+                    newTip = "X2";
+                }
+
+                return {
+                    ...doc,
+                    date: doc.siku,
+                    tip: newTip,
+                    vip_no: 6,
+                    odd: '1'
+                };
+            });
+
+            if (transformedData.length > 0) {
+                await paidVipModel.insertMany(transformedData);
+            }
+        }
     } catch (error) {
         sendNotification(741815228, `❌ Updating VIP Slips \n${error?.message}`)
         console.error(error)
