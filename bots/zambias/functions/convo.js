e6b7370f245a51d1a006e81d7ef6ce397dd1d461
Shemdoe <georgehmariki@gmail.com>
2025-05-15 01:16:49 +0300

    done

--- bots/zambias/functions/convo.js
+++ bots/zambias/functions/convo.js
@@ -18,7 +18,7 @@ const makeConvo = async (bot, ctx, imp) => {
     const bads = ['deactivated', 'blocked', 'initiate', 'chat not found'];
 
     try {
-        const all_users = await usersModel.find({ botname: ctx.me.username });
+        const all_users = await usersModel.find({ botname: ctx.me.username }).select('chatid');
         await ctx.reply(`🚀 Starting broadcasting for ${all_users.length} users`);
 
         for (const user of all_users) {
