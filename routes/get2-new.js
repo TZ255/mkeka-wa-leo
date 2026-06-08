5e852470143df9e49f48c440b3a4c62bc2ae767a
Shemdoe <georgehmariki@gmail.com>
2026-06-08 19:57:06 +0300

    testing djm

--- routes/get2-new.js
+++ routes/get2-new.js
@@ -511,7 +511,7 @@ router.get('/api/testing', async (req, res) => {
     try {
         //await syncOddsForDate('2026-04-01');
         //await GET_TIPS_FOR_MKEKALEO("2026-04-04")
-        // DJMwangaFn("https://djmwanga.com/category/audio")
+        DJMwangaFn("https://djmwanga.com/category/audio")
         // await UpdateMatchDayLeagueData()
         res.json({ok: true})
     } catch (error) {
